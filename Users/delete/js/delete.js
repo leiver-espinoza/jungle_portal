815153ba97fb30c2f4b30d288993cdc5524709f2
siles23 <109173016+siles23@users.noreply.github.com>
2023-03-13 20:51:32 -0600

    cambios

---
+++ Users/delete/js/delete.js
@@ -0,0 +1,28 @@
+const form = document.getElementById("user-form");
+
+form.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  const id = document.getElementById("id").value;
+
+  user = {
+    param_token_owner: sessionStorage.getItem("token_owner"),
+    param_token_value: sessionStorage.getItem("token_value"),
+    param_id: id,
+  };
+  console.log(user);
+
+  const url = "https://api.toolsformyjob.com/users/delete";
+
+  const options = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  };
+
+  fetch(url, options)
+    .then((response) => response.json())
+    .then((response) => console.log(response.data))
+    .catch((err) => console.log(err));
+});
--- Users/read/js/read.js
+++ Users/read/js/read.js
@@ -1,22 +1,28 @@
-const printData = document.getElementById("printData");
+const form = document.getElementById("user-form");
 
-user = {
-  param_token_owner: sessionStorage.getItem("token_owner"),
-  param_token_value: sessionStorage.getItem("token_value"),
-};
+form.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  const id = document.getElementById("id").value;
 
-let inputId = prompt("ID");
-console.log(inputId);
+  user = {
+    param_token_owner: sessionStorage.getItem("token_owner"),
+    param_token_value: sessionStorage.getItem("token_value"),
+    param_id: id,
+  };
+  console.log(user);
 
-const url =
-  "https://api.toolsformyjob.com/users/read?param_token_owner=" +
-  user.param_token_owner +
-  "&param_token_value=" +
-  user.param_token_value +
-  "&param_id=" +
-  inputId;
+  const url =
+    "https://api.toolsformyjob.com/users/read?param_token_owner=" +
+    user.param_token_owner +
+    "&param_token_value=" +
+    user.param_token_value +
+    "&param_id=" +
+    user.param_id;
 
-fetch(url)
-  .then((response) => response.json())
-  .then((response) => console.log(response.data))
-  .catch((err) => console.log(err));
+  fetch(url, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((response) => console.log(response.data))
+    .catch((err) => console.log(err));
+});
