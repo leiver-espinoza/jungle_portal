29cbadc3e5086c703c622ccdbe5db8eff842ba06
Leiver Espinoza <lespinozab@edu.upolitecnica.cr>
2023-03-15 13:02:04 -0600

    Updated URL Generation for ReadAll requests

--- Users/read_all/js/read_all.js
+++ Users/read_all/js/read_all.js
@@ -14,26 +14,26 @@ form.addEventListener("submit", async function (event) {
     name_last: name_last,
     email: email,
     username: username,
-    enabled: true,
+    enabled: true,  // ESTE VALOR NO PUEDE IR POR DEFAULT AQUI EN EL CODIGO, DEBE REALIZAR LA CONSULTA CON BASE A ALGUN CAMPO (PENDIENTE DE AGREGAR)
   };
   console.log(user);
 
   const url =
     "https://api.toolsformyjob.com/users/read_all?param_token_owner=" +
-    user.param_token_owner +
+    user.token_owner +
     "&param_token_value=" +
-    user.param_token_value +
-    "&name_first=" +
+    user.token_value +
+    "&param_name_first=" +
     user.name_first +
-    "&name_last=" +
+    "&param_name_last=" + // Corregido
     user.name_last +
-    "&email=" +
+    "&param_email=" + // Corregido
     user.email +
-    "&username=" +
+    "&param_username=" + // Corregido
     user.username +
-    "&enabled=" +
+    "&param_enabled=" + // Corregido
     user.enabled;
-
+  console.log(url)
   fetch(url, {
     method: "GET",
   })
